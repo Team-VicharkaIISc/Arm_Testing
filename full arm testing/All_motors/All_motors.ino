3a273b019e2cc0b18aab138d5a789a2e22fa5a82
PrathamGupta423 <Prathamgupta423@gmail.com>
2024-01-16 01:17:30 +0530

    Create All_motors.ino
    Added codes for each motor

---
+++ full arm testing/All_motors/All_motors.ino
@@ -0,0 +1,21 @@
+int BR_P = 6;
+int BR_D = 7; 
+int EE_P = 3;
+int EE_D = 4;
+int BE_P = 11;
+int BE_D = 13;
+int ER_P = 9;
+int ER_D = 7;
+int WE_P = 10;
+int WE_D = 12;
+int GR_P = 2;
+
+void setup() {
+  
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
